7719d5ea906a6719cd996812a3119a36c957edc6
Nitesh <nitesh@Devs-MBP.domain.name>
2021-06-24 16:48:18 +0545

    initiala-4

--- src/pages/projects/Projects.js
+++ src/pages/projects/Projects.js
@@ -112,7 +112,7 @@ class Projects extends Component {
         <Button
           text={"More Projects"}
           className="project-button"
-          href="https://github.com/rohankokkula"
+          href="https://github.com/nitesh-tekkon/"
           newTab={true}
           theme={theme}
         />
